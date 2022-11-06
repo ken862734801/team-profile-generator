4beb58ba81dd6c70b928b32098f262f762c24038
ken862734801 <103093259+ken862734801@users.noreply.github.com>
2022-11-05 21:13:24 -0400

    add template html, and add questions based on role

---
+++ app.js
@@ -0,0 +1,83 @@
+
+const managerQuestions = [
+    {
+        type: "input",
+        name: "fName",
+        message: "What is the manager's first name?"
+    },
+    {
+        type: "input",
+        name: "lName",
+        message: "What is the manager's last name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the manager's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the manager's email?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is the manager's office number?"
+    }
+];
+
+const engineerQuestions = [
+    {
+        type: "input",
+        name: "fName",
+        message: "What is the engineer's first name?"
+    },
+    {
+        type: "input",
+        name: "lName",
+        message: "What is the engineer's last name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the engineer's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the engineers's email?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "What is the engineer's GitHub username?"
+    }
+];
+const internQuestions = [
+    {
+        type: "input",
+        name: "fName",
+        message: "What is the intern's first name?"
+    },
+    {
+        type: "input",
+        name: "lName",
+        message: "What is the interns's last name?"
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the intern's ID?"
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the intern's email?"
+    },
+    {
+        type: "input",
+        name: "office",
+        message: "Which university does the intern attend?"
+    }
+];
--- index.js
+++ index.js
@@ -1,2 +1,37 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
+
+let team = [];
+
+function addMember() {
+    inquirer
+    .prompt([
+        {
+            name: "name",
+            message: "Enter a team members name."
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "Select this team member's role.",
+            choices: ["Manager", "Engineer", "Intern"]
+        },
+        {
+            name: "id",
+            message: "Enter this team member's id."
+        },
+        {
+            name: "email",
+            message: "Enter this team member's email."
+        }
+    ])
+    .then(function({response}){
+        console.info(response)
+        if(response.role === "Manager"){
+            
+        }
+    })
+};
+
+addMember();
+
